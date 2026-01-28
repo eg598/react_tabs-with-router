baf1ce181f2b55c8447699cc20b4cee6ee9845a6
Yegor <egor598000@gmail.com>
2026-01-28 03:32:39 +0100

    solution

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { tabs } from '../../tabs';
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+
+  return (
+    <div className="section">
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          <li data-cy="Tab" className={tabId === 'tab-1' ? 'is-active' : ''}>
+            <NavLink
+              to="tab-1"
+              relative="path"
+              className={({ isActive }) => (isActive ? 'is-active' : '')}
+            >
+              Tab 1
+            </NavLink>
+          </li>
+
+          <li data-cy="Tab" className={tabId === 'tab-2' ? 'is-active' : ''}>
+            <NavLink
+              to="tab-2"
+              relative="path"
+              className={({ isActive }) => (isActive ? 'is-active' : '')}
+            >
+              Tab 2
+            </NavLink>
+          </li>
+          <li data-cy="Tab" className={tabId === 'tab-3' ? 'is-active' : ''}>
+            <NavLink
+              to="tab-3"
+              relative="path"
+              className={({ isActive }) => (isActive ? 'is-active' : '')}
+            >
+              Tab 3
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === tabId)?.content || 'Please select a tab'}
+      </div>
+    </div>
+  );
+};
