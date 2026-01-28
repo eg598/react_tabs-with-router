9870d0be3ee39786e24cea6e7836b786a52ee453
Yegor <egor598000@gmail.com>
2026-01-28 03:46:58 +0100

    fix

--- src/components/TabsPage/TabsPage.tsx
+++ src/components/TabsPage/TabsPage.tsx
@@ -10,34 +10,11 @@ export const TabsPage: React.FC = () => {
       <h1 className="title">Tabs page</h1>
       <div className="tabs is-boxed">
         <ul>
-          <li data-cy="Tab" className={tabId === 'tab-1' ? 'is-active' : ''}>
-            <Link
-              to="/tabs/tab-1"
-              relative="path"
-              className={tabId === 'tab-1' ? 'is-active' : ''}
-            >
-              Tab 1
-            </Link>
-          </li>
-
-          <li data-cy="Tab" className={tabId === 'tab-2' ? 'is-active' : ''}>
-            <Link
-              to="/tabs/tab-2"
-              relative="path"
-              className={tabId === 'tab-1' ? 'is-active' : ''}
-            >
-              Tab 2
-            </Link>
-          </li>
-          <li data-cy="Tab" className={tabId === 'tab-3' ? 'is-active' : ''}>
-            <Link
-              to="/tabs/tab-3"
-              relative="path"
-              className={tabId === 'tab-1' ? 'is-active' : ''}
-            >
-              Tab 3
-            </Link>
-          </li>
+          {tabs.map(tab => (
+            <li key={tab.id} className={tabId === tab.id ? 'is-active' : ''}>
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
 
